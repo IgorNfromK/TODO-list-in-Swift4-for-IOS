2b8060a9771fad392b8ac6f0ef43f5a7010b767a
IgorNfromK <ccna4igor@gmail.com>
2019-07-25 22:10:49 +0200

    Initial Commit

---
+++ TODO APP/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TODO APP
+//
+//  Created by Igor Nagaievskyi on 25/07/2019.
+//  Copyright © 2019 Igor Nagaievskyi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TODO APP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TODO APP
+//
+//  Created by Igor Nagaievskyi on 25/07/2019.
+//  Copyright © 2019 Igor Nagaievskyi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
